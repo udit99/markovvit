cec6570d7b587d70606d1704a0c9d732805b16d4
Udit Manektala <code@getbraintree.com>
2016-07-08 14:34:38 -0700

    adding configurations

--- runner.py
+++ runner.py
@@ -1,9 +1,12 @@
+import os
 from flask import Flask
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route("/")
 def hello():
+    print(os.environ['APP_SETTINGS'])
     return "Hello World"
 
 if __name__ == "__main__":
-    foo.run()
+    app.run()
