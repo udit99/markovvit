edc7f7ddde699265436eef42a538a77f4cab1638
Udit Manektala <code@getbraintree.com>
2016-07-08 15:33:55 -0700

    Adding an index.html

--- runner.py
+++ runner.py
@@ -1,12 +1,13 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
-@app.route("/")
-def hello():
+@app.route("/", methods=["GET", "POST"])
+def index():
     print(os.environ['APP_SETTINGS'])
-    return "Hello World"
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run()
