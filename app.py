115360208811b9a81e1c26ac03923ac5723a5940
Udit Manektala <code@getbraintree.com>
2016-08-05 12:29:47 -0700

    Adding redis...splitting out worker...but worker is not working. Investigating

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from stop_words import stops
 from flask.ext.sqlalchemy import SQLAlchemy
 from collections import Counter
 from bs4 import BeautifulSoup
+from rq import Queue
+from rq.job import Job
+from worker import conn
 
 from flask import Flask, render_template, request
 app = Flask(__name__)
@@ -14,12 +17,12 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
+q = Queue(connection=conn)
+
 from models import *
 
-@app.route("/", methods=["GET", "POST"])
-def index():
+def _count_and_save_words(url):
     errors = []
-    results = {}
     r = None
     if request.method == "POST":
         try:
@@ -57,10 +60,35 @@ def index():
                 )
                 db.session.add(result)
                 db.session.commit()
+                return result.id
             except:
                 errors.append("Unable to add item to database.")
-    return render_template('index.html', errors=errors, results=results)
 
+@app.route("/", methods=["GET", "POST"])
+def index():
+    results = {}
+    if request.method == "POST":
+        # get url that the person has entered
+        url = request.form['url']
+        if 'http://' not in url[:7]:
+            url = 'http://' + url
+        job = q.enqueue_call(
+            import pdb;pdb.set_trace()
+            func=_count_and_save_words, args=(url,), result_ttl=5000
+        )
+        print(job.get_id())
+
+        return render_template('index.html', results=results)
+
+@app.route("/results/<job_key>", methods=['GET'])
+def get_results(job_key):
+
+    job = Job.fetch(job_key, connection=conn)
+
+    if job.is_finished:
+        return str(job.result), 200
+    else:
+        return "Nay!", 202
 
 if __name__ == "__main__":
     app.run()
